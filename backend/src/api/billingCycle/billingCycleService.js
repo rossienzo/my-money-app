14aea30725ed2ac3ba710a2a47702bc1ca51d11d
sucutupador <enzorossi97@hotmail.com>
2021-07-06 16:30:54 -0300

    Correção para o problema com as rotas do tipo GET

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -6,4 +6,20 @@ BillingCycle.methods(['get', 'post', 'put', 'delete']);
 // o new retorna as alterações do update no banco e o runValidators valida as atualizações
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+
+// Correção para o problema com as rotas do tipo GET
+BillingCycle.route('get', (req, resp, next) => {
+
+    BillingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+            resp.json(docs)
+        } else {
+            resp.status(500).json({errors: [error]})
+        }
+
+    })
+
+})
+
 module.exports = BillingCycle;
