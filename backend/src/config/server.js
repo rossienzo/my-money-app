7ce40a7498e6e46155927a7c5699d95be2a5a043
sucutupador <enzorossi97@hotmail.com>
2021-07-07 10:41:57 -0300

    Foi adicionado a paginação no servidor. Backend finalizado

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -8,6 +8,9 @@ const server = express();
 // habilita o cors
 const allowCors = require('./cors');
 
+const queryParser = require('express-query-int');
+
+
 // Application port
 const port = 3003;
 
@@ -23,6 +26,7 @@ server.use(function (req, res, next) {
 server.use(bodyParser.urlencoded({ extended: true}));
 server.use(bodyParser.json());
 server.use(allowCors); // faz uso do cors
+server.use(queryParser); // faz uso do recurso de paginação
 
 server.listen(port, function() {
     console.log(`Backend na porta ${port}`);
