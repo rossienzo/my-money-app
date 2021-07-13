bc3de01d7557ae3bd4ac10d978910952fa5aeda5
sucutupador <enzorossi97@hotmail.com>
2021-07-13 11:20:39 -0300

    foi definido o reducer dentro de reducers.js e a aplicação foi configurada em index.jsx com a criação da store

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import App from './main/app';
 
+// Reducer
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './main/reducers';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 );
