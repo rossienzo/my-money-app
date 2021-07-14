e9ab8b2a4e932ae6a38bec9e68ac49fdb329086a
sucutupador <enzorossi97@hotmail.com>
2021-07-14 13:51:25 -0300

    Conectei o redux om as tabs

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class TabHeader extends Component {
+import { selectTab } from './tabActions';
+
+class TabHeader extends Component {
+
+    componentDidMount() {
+        this.props.selectTab;
+    }
 
     render() {
+
+        const selected = this.props.tab.selected === this.props.target
+
         return (
-            <li>
+            <li className={selected ? 'active' : ''}>
                 {/* O comando javascript:; ignora qualquer click do botão */}
-                <a href="javascript:;" data-toggle="tab" data-target={this.props.target}>
+                <a href="javascript:;" 
+                   data-toggle="tab" 
+                   data-target={this.props.target}
+                   onClick={() => this.props.selectTab(this.props.target)}>
                     <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                 </a>
             </li>
         )
     }
 }
+
+const mapStateToProps = state => ({ tab: state.tab}); // pega o estado do reduer tab em reducers.js
+const mapDispathToProps = dispatch => bindActionCreators({selectTab}, dispatch);
+
+export default connect(mapStateToProps, mapDispathToProps)(TabHeader);
